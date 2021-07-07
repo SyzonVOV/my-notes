7872a7702269dfbbe00a927f464d659a7febcc25
SyzonVOV <sizonenko89@gmail.com>
2021-07-07 15:47:36 +0300

    chore: Create reducer
    Created reducer with the possibility to create and delete notes

---
+++ src/store/reducer.ts
@@ -0,0 +1,48 @@
+//A reducer is a pure function that receives the state of the store and an action as parameters and then returns the updated state.
+import * as actionTypes from "./actionTypes"
+
+const initialState: NoteState = {
+  notes: [
+    {
+      id: 1,
+      title: "note 1",
+      body:
+        "Quisque cursus, metus vitae pharetra Nam libero tempore, cum soluta nobis est eligendi",
+    },
+    {
+      id: 2,
+      title: "note 2",
+      body:
+        "Harum quidem rerum facilis est et expedita distinctio quas molestias excepturi sint",
+    },
+  ],
+}
+
+const reducer = (
+  state: NoteState = initialState,
+  action: NoteAction
+): NoteState => {
+  switch (action.type) {
+    case actionTypes.ADD_NOTE:
+      const newNote: INote = {
+        id: Math.random(),
+        title: action.note.title,
+        body: action.note.body,
+      }
+      return {
+        ...state,
+        notes: state.notes.concat(newNote),
+      }
+    case actionTypes.REMOVE_NOTE:
+      const updatedNotes: INote[] = state.notes.filter(
+        note => note.id !== action.note.id
+      )
+      return {
+        ...state,
+        notes: updatedNotes,
+      }
+  }
+  return state
+}
+
+export default reducer
