74da8d1ce3c855bf0faf563f3998ed42932bbe20
SyzonVOV <sizonenko89@gmail.com>
2021-07-07 15:22:43 +0300

    chore: Make types for app

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,1 +1,18 @@
 // The type.d.ts file that holds the TypeScript types, which can be used now in other files without importing.
+
+interface INote {
+  id: number
+  title: string
+  body: string
+}
+
+type NoteState = {
+  notes: INote[]
+}
+
+type NoteAction = {
+  type: string
+  note: INote
+}
+
+type DispatchType = (args: NoteAction) => NoteAction
