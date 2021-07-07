b19a595793f652c450b3092bee3890463564cb4c
SyzonVOV <sizonenko89@gmail.com>
2021-07-07 15:30:33 +0300

    chore: Create the actions
    Created actions types and action creators in the respective files

---
+++ src/store/actionCreators.ts
@@ -0,0 +1,26 @@
+import * as actionTypes from "./actionTypes"
+
+export function addNote(note: INote) {
+  const action: NoteAction = {
+    type: actionTypes.ADD_NOTE,
+    note,
+  }
+
+  return simulateHttpRequest(action)
+}
+
+export function removeNote(note: INote) {
+  const action: NoteAction = {
+    type: actionTypes.REMOVE_NOTE,
+    note,
+  }
+  return simulateHttpRequest(action)
+}
+
+export function simulateHttpRequest(action: NoteAction) {
+  return (dispatch: DispatchType) => {
+    setTimeout(() => {
+      dispatch(action)
+    }, 500)
+  }
+}
